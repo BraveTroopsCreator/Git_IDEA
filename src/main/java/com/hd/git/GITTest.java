5d6a8933a76bd9a632d24888ccd02ff23b3594d2
Roger <15382800164@163.com>
2021-06-27 13:50:28 +0800

    Git的第一次提交

---
+++ src/main/java/com/hd/git/GITTest.java
@@ -0,0 +1,15 @@
+package com.hd.git;
+
+/**
+ * @Author: Roger
+ * @PackageName: com.hd.git
+ * @ClassName: GITTest
+ * @Description:
+ * @Create: 2021/6/27 13:35
+ * @Version: 1.0
+ */
+public class GITTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
